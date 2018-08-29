36be8a65ebdd1192f8990c19a1f182dd692032be
David Rogers <davidrogers090@gmail.com>
2018-08-28 19:08:42 -0700

    Reorganized the project and added a simple REST endpoint in Java

---
+++ calendar-server/src/main/java/edu/sen602/group/calendarclient/CalendarClientApplication.java
@@ -0,0 +1,12 @@
+package edu.sen602.group.calendarclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarClientApplication.class, args);
+	}
+}
---
+++ calendar-server/src/main/java/edu/sen602/group/calendarclient/rest/HomeController.java
@@ -0,0 +1,16 @@
+package edu.sen602.group.calendarclient.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+	@GetMapping
+	public String home(Model model) {
+		return "forward:/index.html";
+	}
+}
