d9b9478ff52e7ec9099fb91a5182e97b33e9a6dd
Christopher Woodward <christopherwoodward@Christophers-iMac.local>
2018-08-31 13:59:03 -0700

    Created editEventForm editForm method on EventDetailsComponent

--- calendar-client/src/main/web/src/app/app.module.ts
+++ calendar-client/src/main/web/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { EventService } from './services/event.service';
@@ -13,7 +13,9 @@ import { EventDetailComponent } from './calendar/event-detail/event-detail.compo
     EventDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [EventService],
   bootstrap: [AppComponent]
--- calendar-client/src/main/web/src/app/calendar/event-detail/event-detail.component.ts
+++ calendar-client/src/main/web/src/app/calendar/event-detail/event-detail.component.ts
@@ -1,5 +1,6 @@
 import { Event } from './../../types/event';
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-event-detail',
@@ -7,11 +8,55 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./event-detail.component.css']
 })
 export class EventDetailComponent implements OnInit {
-  @Input() events: Event;
+  @Input() events: Event[];
+  private editEventForm: FormGroup;
+  private createEventForm: FormGroup;
+  private editingEvent: Event = null;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.editEventForm = new FormGroup({
+      'name': new FormControl(null, [Validators.required]),
+      'time': new FormControl(null, [Validators.required]),
+      'location': new FormControl(null, [Validators.required]),
+      'notes': new FormControl(null)
+    });
+    // this.createEventForm = new FormGroup({
+    //   'name': new FormControl(null, [Validators.required]),
+    //   'time': new FormControl(null, [Validators.required]),
+    //   'location': new FormControl(null, [Validators.required]),
+    //   'notes': new FormControl(null)
+    // });
   }
 
+  ngOnInit() {}
+
+  onSubmit() {
+    const newEvent = {
+      id: this.editingEvent.id,
+      name: this.editingEvent.name,
+      datetime: this.editingEvent.datetime,
+      location: this.editingEvent.location,
+      notes: this.editingEvent.notes,
+      duration: this.editingEvent.duration
+    };
+    const formattedDatetime = this.formatDatetime(this.editEventForm.get('time').value);
+    console.log(this.editEventForm.get('name'));
+  }
+
+  formatDatetime(timeInHours: number): string {
+    const eventDate = this.events[0].datetime;
+    return "";
+  }
+
+  setEditingEvent(id: number): void {
+    this.events.map(event => {
+      if (event.id === id) {
+        this.editingEvent = event;
+      }
+    });
+  }
+
+  closeEditingEvent() {
+    this.editingEvent = null;
+  }
 }
